f7370189416b81b74fdfc58c038580d421952051
justinwilliamsrva <justin.williams2@me.com>
2021-04-16 05:59:22 -0400

    feat!:Setup outline for nav bar with background and link text

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <div className="header">
+        <div className="header-content">
+          <h3>Home</h3>
+          <h3>Skills</h3>
+          <h3>Projects</h3>
+          <h1 className="name">Justin Williams</h1>
+          <h3>Resume</h3>
+          <h3>Music</h3>
+          <h3>Contact</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
