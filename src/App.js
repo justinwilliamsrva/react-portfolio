2b2771cdbec058523e7024e1101f0a7dccafd3cf
justinwilliamsrva <justin.williams2@me.com>
2021-04-19 14:57:23 -0400

    feat: created home component - added profile, name and title
    Added the Justin-min jpg, created css varaible for primary and secondary colors, and added secondary color to title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="home">
+        <div className="personal">
+          <img id="profile" src="/Justin-min.jpg" />
+          <h2>Justin Williams</h2>
+          <h3>Full-Stack Web Developer</h3>
+        </div>
+        <div className="biography"></div>
+      </div>
+    </div>
+  );
+}
