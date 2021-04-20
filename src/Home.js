78949e2c19e14159007104fefe5aedc62521241c
justinwilliamsrva <justin.williams2@me.com>
2021-04-20 06:01:02 -0400

    feat: added biogrphy and edcuation sections with Lorem

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,46 @@ export default function Home() {
             <img className="icons-icon" src="/linkedin.png" />
           </div>
         </div>
-        <div className="biography"></div>
+        <div className="biography">
+          <div id="bio">
+            <h2>Biography</h2>
+            <p>
+              <b>
+                Proident veniam sunt elit Lorem sit ullamco. Reprehenderit ex
+                deserunt magna officia cillum qui. Non sint culpa cupidatat duis
+                ad in proident magna nulla minim consequat elit. Non et laborum
+                anim amet sint Lorem velit mollit elit reprehenderit excepteur.
+                Aute veniam sunt occaecat tempor ut voluptate.
+              </b>
+            </p>
+            <p>
+              <b>
+                Voluptate officia eu ut excepteur nisi eu excepteur aliquip eu
+                officia non enim incididunt. Dolor culpa excepteur reprehenderit
+                qui tempor eu sunt eu excepteur excepteur aute minim. Magna
+                magna anim reprehenderit aute reprehenderit nisi ea. Occaecat
+                tempor magna veniam exercitation consequat officia enim
+                consequat ea. Id do eiusmod minim consequat nostrud elit aute
+                consectetur elit. Duis consequat id voluptate veniam consequat.
+              </b>
+            </p>
+          </div>
+          <div id="education">
+            <h2>Education</h2>
+            <div className="school">
+              <p>
+                <b>Full Stack Developer Boot Camp</b>
+              </p>
+              <p>University of Richmond</p>
+            </div>
+            <div className="school">
+              <p>
+                <b>BA in Music Education</b>
+              </p>
+              <p>Virginia Commonwealth University</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
