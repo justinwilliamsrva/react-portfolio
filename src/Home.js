33d778a9633895d51aafce2a215c9c057565ea45
justinwilliamsrva <justin.williams2@me.com>
2021-04-20 05:36:05 -0400

    feat: personal side of Home icons added from figma

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,12 @@ export default function Home() {
           <img id="profile" src="/Justin-min.jpg" />
           <h2>Justin Williams</h2>
           <h3>Full-Stack Web Developer</h3>
+          <div id="icons">
+            <img className="icons-icon" src="/mail_icon.png" />
+            <img className="icons-icon" src="/github.png" />
+            <img className="icons-icon" src="/twitter.png" />
+            <img className="icons-icon" src="/linkedin.png" />
+          </div>
         </div>
         <div className="biography"></div>
       </div>
