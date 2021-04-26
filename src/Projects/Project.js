e70f05ad2aaf5671f3bd9de2d9b30a21ac23e161
justinwilliamsrva <justin.williams2@me.com>
2021-04-26 05:31:09 -0400

    feat: in Projects - added a map over techs and added links for site and github

--- src/Projects/Project.js
+++ src/Projects/Project.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 export default function Project({ props }) {
+  const techs = props.techs;
+  console.log(techs);
+
   return (
     <div>
       {props.id % 2 === 1 ? (
@@ -10,16 +13,38 @@ export default function Project({ props }) {
           </div>
           <div className="card-right">
             <h3>{props.title}</h3>
+            <ul className="project-link-list">
+              <li className="project-link">
+                <a href={props.link} target="_blank">
+                  Check it Out
+                </a>
+              </li>
+              <li className="project-link">
+                <a href={props.github} target="_blank">
+                  Github
+                </a>
+              </li>
+            </ul>
             <p>{props.description}</p>
-            {/* <p>{props.technologies}</p> */}
+
+            <ul className="tech-list">
+              {techs.map((tech) => (
+                <li className="tech">{tech}</li>
+              ))}
+            </ul>
           </div>
         </div>
       ) : (
         <div className="project-card-reverse">
           <div className="card-right">
             <h3>{props.title}</h3>
+
             <p>{props.description}</p>
-            {/* <p>{props.technologies}</p> */}
+            <ul className="tech-list">
+              {techs.map((tech) => (
+                <li className="tech">{tech}</li>
+              ))}
+            </ul>
           </div>
           <div className="card-left">
             <img className="project-image" src={props.img} alt="images" />
--- src/Projects/ProjectList.js
+++ src/Projects/ProjectList.js
@@ -6,8 +6,11 @@ const projects = [
     id: 1,
     title: "Spirited Scenery",
     description:
-      "Ullamco Lorem amet quis consequat consectetur deserunt occaecat. Sint ea labore magna minim irure quis dolore excepteur fugiat ea aliqua. Proident adipisicing excepteur quis officia exercitation id laborum. Cillum labore Lorem dolor nostrud cillum et occaecat culpa. Duis duis est ad exercitation minim officia culpa. Magna ad pariatur ullamco sunt commodo velitDolor in non consectetur voluptate est Lorem et commodo aliquip ad voluptate irure exercitation in. Nostrud sit voluptate pariatur enim Lorem. Ipsum magna fugiat occaecat velit minim cupidatat enim officia eiusmod in ex. Cillum labore dolor mollit sunt. Elit proident labore eu pariatur consequat. Occaecat exercitation eu anim ipsum ut minim.",
+      "Explore your city's culture confidently with the Spirited Scenery App! Use our app to easily find local art museums and breweries! In addition, the app considers your local weather conditions and offers advice on how to whether you should venture on foot or wear a coat on your travels. If you're an art sophisticate, critic, a bar crawler or all of the above, we've got you covered!",
     img: "/Spirted.JPG",
+    techs: ["JQuery", "Material-UI", "AJAX"],
+    link: "https://justinwilliamsrva.github.io/Spirited-Scenery/",
+    github: "https://github.com/justinwilliamsrva/Spirited-Scenery",
   },
   {
     id: 2,
@@ -15,6 +18,7 @@ const projects = [
     description:
       "Ullamco Lorem amet quis consequat consectetur deserunt occaecat. Sint ea labore magna minim irure quis dolore excepteur fugiat ea aliqua. Proident adipisicing excepteur quis officia exercitation id laborum. Cillum labore Lorem dolor nostrud cillum et occaecat culpa. Duis duis est ad exercitation minim officia culpa. Magna ad pariatur ullamco sunt commodo velitDolor in non consectetur voluptate est Lorem et commodo aliquip ad voluptate irure exercitation in. Nostrud sit voluptate pariatur enim Lorem. Ipsum magna fugiat occaecat velit minim cupidatat enim officia eiusmod in ex. Cillum labore dolor mollit sunt. Elit proident labore eu pariatur consequat. Occaecat exercitation eu anim ipsum ut minim.",
     img: "rpg-photo.JPG",
+    techs: ["JQuery", "Material-UI", "AJAX"],
   },
   {
     id: 3,
@@ -22,6 +26,7 @@ const projects = [
     description:
       "Ullamco Lorem amet quis consequat consectetur deserunt occaecat. Sint ea labore magna minim irure quis dolore excepteur fugiat ea aliqua. Proident adipisicing excepteur quis officia exercitation id laborum. Cillum labore Lorem dolor nostrud cillum et occaecat culpa. Duis duis est ad exercitation minim officia culpa. Magna ad pariatur ullamco sunt commodo velitDolor in non consectetur voluptate est Lorem et commodo aliquip ad voluptate irure exercitation in. Nostrud sit voluptate pariatur enim Lorem. Ipsum magna fugiat occaecat velit minim cupidatat enim officia eiusmod in ex. Cillum labore dolor mollit sunt. Elit proident labore eu pariatur consequat. Occaecat exercitation eu anim ipsum ut minim.",
     img: "cavorch.JPG",
+    techs: ["Javascript", "JQuery", "Material-UI", "AJAX"],
   },
 ];
 export function ProjectList() {
