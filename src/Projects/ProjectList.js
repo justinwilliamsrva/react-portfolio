8811b6e1ccb468b52c3e32d7e557783a7a66e172
justinwilliamsrva <justin.williams2@me.com>
2021-06-25 09:49:38 -0400

    feat: Added new projects to the project page

--- src/Projects/ProjectList.js
+++ src/Projects/ProjectList.js
@@ -16,17 +16,42 @@ const projects = [
     id: 2,
     title: "Orchestra RPG",
     description:
-      "Ullamco Lorem amet quis consequat consectetur deserunt occaecat. Sint ea labore magna minim irure quis dolore excepteur fugiat ea aliqua. Proident adipisicing excepteur quis officia exercitation id laborum. Cillum labore Lorem dolor nostrud cillum et occaecat culpa. Duis duis est ad exercitation minim officia culpa. Magna ad pariatur ullamco sunt commodo velitDolor in non consectetur voluptate est Lorem et commodo aliquip ad voluptate irure exercitation in. Nostrud sit voluptate pariatur enim Lorem. Ipsum magna fugiat occaecat velit minim cupidatat enim officia eiusmod in ex. Cillum labore dolor mollit sunt. Elit proident labore eu pariatur consequat. Occaecat exercitation eu anim ipsum ut minim.",
+      "Created to motivate orchestra students to learn scales, shifting and bow technique during mandated virtual learningStudents can login to profile, turn-in assignments, receive teacher feedback, compare progress to the rest of the class.",
     img: "rpg-photo.JPG",
-    techs: ["JQuery", "Material-UI", "AJAX"],
+    techs: ["HTML", "CSS", "JavaScript"],
+    link: "https://cavorch.org/orchestra/rpg.html",
+    github:
+      "https://github.com/justinwilliamsrva/chhsorchestra/tree/master/orchestra",
   },
   {
     id: 3,
     title: "CHHS Orchestra",
     description:
-      "Ullamco Lorem amet quis consequat consectetur deserunt occaecat. Sint ea labore magna minim irure quis dolore excepteur fugiat ea aliqua. Proident adipisicing excepteur quis officia exercitation id laborum. Cillum labore Lorem dolor nostrud cillum et occaecat culpa. Duis duis est ad exercitation minim officia culpa. Magna ad pariatur ullamco sunt commodo velitDolor in non consectetur voluptate est Lorem et commodo aliquip ad voluptate irure exercitation in. Nostrud sit voluptate pariatur enim Lorem. Ipsum magna fugiat occaecat velit minim cupidatat enim officia eiusmod in ex. Cillum labore dolor mollit sunt. Elit proident labore eu pariatur consequat. Occaecat exercitation eu anim ipsum ut minim.",
+      "Static website that displays the orchestra, conductor information, orchestra news and the calendar  parents and includes important links for students",
     img: "cavorch.JPG",
-    techs: ["Javascript", "JQuery", "Material-UI", "AJAX"],
+    techs: ["HTML", "CSS", "JavaScript"],
+    link: "https://cavorch.org/",
+    github: "https://github.com/justinwilliamsrva/chhsorchestra",
+  },
+  {
+    id: 4,
+    title: "Summer Camp Staff App",
+    description:
+      "A CRUD app that allows camp organizers and staff to keep track of inventory, manage student records, and communicate with all staff while keeping certain features reserved for authorized users",
+    img: "staff.JPG",
+    techs: ["MongoDB", "Express", "React"],
+    link: "https://summercampstaffapp.herokuapp.com/",
+    github: "https://github.com/justinwilliamsrva/SummerCampApp",
+  },
+  {
+    id: 5,
+    title: "Violin Central",
+    description:
+      "A CRUD app where authenticated users can access a Violin Book Database and post new entries. Users can also search for lessons plans and connected them to certain books",
+    img: "violin central.JPG",
+    techs: ["Handlebars", "Express", "MySQL"],
+    link: "http://violin-central.herokuapp.com/",
+    github: "https://github.com/justinwilliamsrva/Violin-Central",
   },
 ];
 export function ProjectList() {
