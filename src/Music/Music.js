d1d2ec79349d17c4e9b63787a21d04aa48b7bdac
justinwilliamsrva <justin.williams2@me.com>
2021-06-25 14:53:32 -0400

    feat!: changes styling to music sections. added correct links

--- src/Music/Music.js
+++ src/Music/Music.js
@@ -3,61 +3,44 @@ import Video from "./Video";
 const videos = [
   {
     id: 1,
-    song: "asdasd",
-    composer: "asdas",
-    date: "May 23, 1988",
+    song: "Jesus Paid it All",
+    composer: "Grape/Gustafson",
+    date: "October 1, 2017",
     photo: "/MV-Arise-my-soul.jpg",
     link: "https://vimeo.com/236775991",
   },
   {
     id: 2,
-    song: "asdasd",
-    composer: "asdas",
-    date: "asdasdad",
-    photo: "/MV-Arise-my-soul.jpg",
-    link: "https://vimeo.com/236775991",
-  },
-
-  {
-    id: 3,
-    song: "asdasd",
-    composer: "asdas",
-    date: "asdasdad",
-    photo: "/MV-Arise-my-soul.jpg",
-    link: "https://vimeo.com/236775991",
-  },
-  {
-    id: 4,
-    song: "asdasd",
-    composer: "asdas",
-    date: "May 23, 1988",
-    photo: "/MV-Arise-my-soul.jpg",
-    link: "https://vimeo.com/236775991",
-  },
-  {
-    id: 5,
-    song: "asdasd",
-    composer: "asdas",
-    date: "asdasdad",
-    photo: "/MV-Arise-my-soul.jpg",
-    link: "https://vimeo.com/236775991",
-  },
-
-  {
-    id: 6,
     song: "Chesterfield Observer Article",
     composer: "Rich Griset",
     date: "February 7, 2018",
     photo: "/article.jpg",
-    link:
-      "https://www.chesterfieldobserver.com/articles/at-clover-hill-a-youth-string-orchestra-preps-for-its-debut/",
+    link: "https://www.chesterfieldobserver.com/articles/at-clover-hill-a-youth-string-orchestra-preps-for-its-debut/",
+  },
+
+  {
+    id: 3,
+    song: "Concertant Duet Op.57 No.3: Moderato",
+    composer: "Charles Auguste de Beriot ",
+    date: "February 28, 2021 ",
+    photo: "beriot.jpg",
+    link: "https://www.youtube.com/watch?v=9X4zD4wjbnM",
+  },
+
+  {
+    id: 4,
+    song: "Passacaglia",
+    composer: "Handel-Halvorsen",
+    date: "May 23, 2021",
+    photo: "/maxresdefault.jpg",
+    link: "https://www.chesterfieldobserver.com/articles/at-clover-hill-a-youth-string-orchestra-preps-for-its-debut/",
   },
 ];
 
 export default function Music() {
   return (
     <div className="section-header" id="music">
-       <span id="musics"></span>
+      <span id="musics"></span>
       <h2>Music</h2>
       <div id="music-videos">
         {videos.map((video) => (
--- src/Music/Video.js
+++ src/Music/Video.js
@@ -4,13 +4,14 @@ export default function Video({ props }) {
   return (
     <div class="container">
       <img src={props.photo} alt={props.song} class="image" />
-      <h3>{props.date}</h3>
+      <h3>
+        {props.song} by {props.composer}{" "}
+      </h3>
       <div class="middle">
-        <h4>{props.song}</h4>
-        <h4>{props.composer}</h4>
-        <a href={props.link} target="_blank" class="text">
-          Watch
+        <a href={props.link} target="_blank" rel="noreferrer" class="text">
+          View
         </a>
+        <h4>{props.date}</h4>
       </div>
     </div>
   );
